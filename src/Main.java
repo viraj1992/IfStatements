e5a8400e04e6a3c337e77cf6561a915f226ca4f6
Viraj Pai <coder.viraj@gmail.com>
2018-01-27 16:24:50 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String stringData = "Hello1";
+		String stringData2 = "OMG";
+
+		if(stringData == "Hello") {
+			System.out.println("String present is: " + stringData); }
+		else if(stringData2 == "OMG"){
+			System.out.println(stringData2 + " Found!!!!");
+		}
+		else {
+			System.out.println("String Hello is not present");
+		}
+		
+	}
+}
